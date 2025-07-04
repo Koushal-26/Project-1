0258b95c98e145f7170dffba54ea5806270a6f2d
Koushal Kumar Sharma <sharmajijkjk@gmail.com>
2025-07-04 15:44:39 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 
 const poppins = localfont({
-  src: "./fonts/poppins-ExtraBold.ttf",
+  src: "./fonts/Poppins-ExtraBold.ttf",
   variable: "--font-poppins",
   weight: "100 900",
   subsets: ["latin"],
